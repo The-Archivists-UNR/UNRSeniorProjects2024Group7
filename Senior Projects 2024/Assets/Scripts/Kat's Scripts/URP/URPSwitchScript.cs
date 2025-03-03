b9346a1daa0d64391764e4ca9c407b2b891b9574
Kat <katw@unr.edu>
2025-03-02 18:29:48 -0800

    im goated
    fixed urp

---
+++ Senior Projects 2024/Assets/Scripts/Kat's Scripts/URP/URPSwitchScript.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class URPSwitchScript : MonoBehaviour
+{
+    public RenderPipelineAsset NoireURP;
+    public void SwitchToNoire()
+    {
+        GraphicsSettings.renderPipelineAsset = NoireURP;
+    }
+
+    public void SwitchToStandard()
+    {
+        GraphicsSettings.renderPipelineAsset = null;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().name == "(Scene 4) Noire")
+        {
+            SwitchToNoire();
+        }
+        else
+        {
+            SwitchToStandard();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
