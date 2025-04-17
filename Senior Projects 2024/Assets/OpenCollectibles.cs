6125865284d15d081deb4e58ed2c22942994158c
Kat <katw@unr.edu>
2025-04-16 18:05:07 -0700

    got collectibles to open

--- Senior Projects 2024/Assets/OpenCollectibles.cs
+++ Senior Projects 2024/Assets/OpenCollectibles.cs
@@ -3,10 +3,48 @@ using UnityEngine;
 //taking code from fenn's book switch 
 public class OpenCollectibles : MonoBehaviour
 {
-    [SerializeField] private GameObject Collectibles;
+    public GameObject Collectibles;
+    public string interactKey = "e";
+    private bool playerInCollider = false;
 
-    private void OnMouseUpAsButton()
+    private void OnTriggerEnter(Collider other)
     {
-        Collectibles.SetActive(true);
+        if (other.CompareTag("Player"))
+        {
+            playerInCollider = true;
+            print("entered collectibles");       
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInCollider = false;
+            if (Collectibles != null)
+            {
+                Collectibles.SetActive(false);
+                print("Closed UI");
+            }
+            print("exited collectibles");
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player") && playerInCollider)
+        {
+            if (Input.GetKeyDown(interactKey))
+            {
+                if (Collectibles != null)
+                {
+                    Collectibles.SetActive(true);
+                    print("Opened UI");
+                }
+                else
+                {
+                    print("What UI?");
+                }
+            }
+        }
     }
 }
