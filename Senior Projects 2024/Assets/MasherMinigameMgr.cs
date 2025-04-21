e605a18238d523cbe658f41fada8c9047af64268
Liam Francisco <98508195+francisco-liam@users.noreply.github.com>
2025-04-21 08:53:25 -0700

    masher ui update

--- Senior Projects 2024/Assets/MasherMinigameMgr.cs
+++ Senior Projects 2024/Assets/MasherMinigameMgr.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MasherMinigameMgr : MonoBehaviour
 {
-    public float timer = 0;
+    public float timer = 100000;
     public float maxTime = 0;
-    public int score;
+    public Slider timerSlider;
+    public int score = 0;
+    public List<int> maxScore;
+    public GameObject menu;
     public RectTransform stamp;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI maxScoreText;
     public float distance;
 
     // Start is called before the first frame update
@@ -23,12 +31,62 @@ public class MasherMinigameMgr : MonoBehaviour
         timer += Time.deltaTime;
         if(timer < maxTime)
         {
-            if(Input.GetKeyUp(KeyCode.Space))
+            menu.SetActive(false);
+            timerSlider.value = timer / maxTime;
+
+            if (Input.GetKeyUp(KeyCode.Space))
             {
                 score++;
+                scoreText.text = "Score: " + score;
+                maxScoreText.text = UpdateHighScore(score);
                 StartCoroutine(BounceImage(stamp, distance, 0.1f));
             }
         }
+        else
+        {
+            timer = 68;
+            menu.SetActive(true);
+        }
+    }
+
+    public void StartGame()
+    {
+        UnityEngine.EventSystems.EventSystem.current.SetSelectedGameObject(null);
+        timer = 0;
+        score = 0;
+        scoreText.text = "Score: " + score;
+    }
+
+    public void UpdateDifficulty(int setting)
+    {
+        Debug.Log(setting);
+
+        if (setting == 0)
+            maxTime = 10;
+        else if (setting == 1)
+            maxTime = 30;
+        else if (setting == 2)
+            maxTime = 60;
+    }
+
+    string UpdateHighScore(int score)
+    {
+        if(maxTime == 10)
+            if(score > maxScore[0])
+                maxScore[0] = score;
+        if (maxTime == 30)
+            if (score > maxScore[1])
+                maxScore[1] = score;
+        if (maxTime == 60)
+            if (score > maxScore[2])
+                maxScore[2] = score;
+
+        string output = "High Scores\n";
+        output += "10s: " + maxScore[0];
+        output += "\n30s: " + maxScore[1];
+        output += "\n60s: " + maxScore[2];
+
+        return output;
     }
 
     private bool isBouncing = false;
@@ -48,11 +106,6 @@ public class MasherMinigameMgr : MonoBehaviour
             image.anchoredPosition = Vector3.Lerp(start, down, t);
             yield return null;
         }
-
-        // Wait 3 frames
-        yield return null;
-        yield return null;
-        yield return null;
 
         // Move back up
         t = 0f;
