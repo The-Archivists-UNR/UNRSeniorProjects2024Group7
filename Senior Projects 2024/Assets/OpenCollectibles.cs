d3c681003c4d33d3b04062e7a53defb909307282
Kat <katw@unr.edu>
2025-04-23 22:52:17 -0700

    finally got the flip animation to work, walking pauses for collectibles ui, made tutorial level

--- Senior Projects 2024/Assets/OpenCollectibles.cs
+++ Senior Projects 2024/Assets/OpenCollectibles.cs
@@ -7,12 +7,26 @@ public class OpenCollectibles : MonoBehaviour
     public string interactKey = "e";
     private bool playerInCollider = false;
 
+    // Pauses the time for when you're in a menu
+    public void pauseTime()
+    {
+        GameEventsManager.instance.playerEvents.DisablePlayerMovement();
+        Time.timeScale = 0;
+    }
+
+    // Starts the time for when you're out of a menu
+    public void startTime()
+    {
+        GameEventsManager.instance.playerEvents.EnablePlayerMovement();
+        Time.timeScale = 1;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             playerInCollider = true;
-            print("entered collectibles");       
+            print("entered collectibles");
         }
     }
 
@@ -39,6 +53,7 @@ public class OpenCollectibles : MonoBehaviour
                 {
                     Collectibles.SetActive(true);
                     print("Opened UI");
+                    pauseTime();
                 }
                 else
                 {
@@ -47,4 +62,6 @@ public class OpenCollectibles : MonoBehaviour
             }
         }
     }
+
+
 }
